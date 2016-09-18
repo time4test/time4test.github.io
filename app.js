81b64acd7343e47de4012c8923e6919e7d790c47
time4test <dviorel@geocities.com>
2016-09-18 18:38:30 -0400

    wk1

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+	    
+function LunchCheckController($scope) {
+  $scope.message = "Enter your lunch items !";
+  $scope.lunch_menu = "";
+  
+  $scope.checkLunch = function () {
+    var totalNameValue = calculateLunch($scope.lunch_menu);
+    $scope.message = totalNameValue;
+  };
+
+
+  function calculateLunch(string) {
+    if (string == "")
+      return "Please enter data first !";
+    var splitString = string.split(/[\s,]+/);
+    if (splitString.length <= 3)
+      return "Enjoy !" + splitString.length;
+    else
+      return "Too Much !" + splitString.length;
+}
+}
+
+})();
+
